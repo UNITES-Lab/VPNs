8060463f0b445695a1c219b55340edc098fd1fd3
jincan333 <jincan333@gmail.com>
2023-09-29 04:05:45 -0400

    revise

--- get_model_dataset.py
+++ get_model_dataset.py
@@ -50,9 +50,9 @@ def get_model(args):
         from torch import hub
         network = hub.load('facebookresearch/WSL-Images', 'resnext101_32x8d_wsl').to(args.device)
     elif args.network == 'vgg':
-        from torchvision.models import vgg16, VGG16_Weights, vgg16_bn
-        # network = vgg16(weights=VGG16_Weights.IMAGENET1K_V1).to(args.device)
-        network = vgg16_bn(pretrained=True)
+        from torchvision.models import vgg16, VGG16_Weights, vgg16_bn, VGG16_BN_Weights
+        network = vgg16(weights=VGG16_Weights.IMAGENET1K_V1).to(args.device)
+        # network = vgg16_bn(weights=VGG16_BN_Weights.IMAGENET1K_V1).to(args.device)
     else:
         raise NotImplementedError(f"{args.network} is not supported")
     
